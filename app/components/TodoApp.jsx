76b25acd61ec4b3fdd86be177a48ce0ac93dc1a6
Alec Jandorek <ajandorek@gmail.com>
2017-07-18 20:46:35 -0500

    Add toggle support

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,16 +12,20 @@ var TodoApp = React.createClass({
             todos: [
                 {
                     id: uuid(),
-                    text: 'Walk the dog'
+                    text: 'Walk the dog',
+                    completed: false
                 },{
                     id: uuid(),
-                    text: 'Clean the yard'
+                    text: 'Clean the yard',
+                    completed: true
                 }, {
                     id: uuid(),
-                    text: 'Save the commonwealth'
+                    text: 'Save the commonwealth',
+                    completed: true
                 }, {
                     id: uuid(),
-                    text: 'Learn React'
+                    text: 'Learn React',
+                    completed: false
                 }
             ]
         };
@@ -37,10 +41,21 @@ var TodoApp = React.createClass({
             ]
         });
     },
+    handleToggle: function(id) {
+        var updatedTodos = this.state.todos.map((todo) => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+
+            return todo;
+        });
+        this.setState({todos: updatedTodos});
+    },
     handleSearch: function(showCompleted, searchText) {
         this.setState({
             showCompleted: showCompleted,
-            searchText: searchText.toLowerCase()
+            searchText: searchText.toLowerCase(),
+            completed: false
         });
     },
     render: function () {
@@ -48,7 +63,7 @@ var TodoApp = React.createClass({
         return (
             <div>
                 <TodoSearch onSearch={this.handleSearch} />
-                <TodoList todos={todos} />
+                <TodoList todos={todos} onToggle={this.handleToggle}/>
                 <TodoForm onAddTodo={this.handleAddTodo}/>
             </div>
         );
