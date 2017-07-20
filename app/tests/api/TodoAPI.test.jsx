abdd04f6cc3264c518b323dc98a0f4f58a799a47
Alec Jandorek <ajandorek@gmail.com>
2017-07-19 21:39:02 -0500

    add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -50,7 +50,7 @@ describe('TodoAPI', () => {
         });
     });
 
-    describe('filterTods', () => {
+    describe('filterTodos', () => {
         var todos = [{
             id: 1,
             text: 'some text here',
@@ -72,6 +72,18 @@ describe('TodoAPI', () => {
         it('should not return all items if showCompleted is false', () => {
             var filteredTodos = TodoAPI.filterTodos(todos, false, '');
             expect(filteredTodos.length).toBe(1);
+        });
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+        it('should filter todos by searchText', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+            expect(filteredTodos.length).toBe(2);
+        });
+        it('should return all todos if searchText is empty', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
         });
     });
 });
